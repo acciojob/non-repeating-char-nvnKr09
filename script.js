dadf0c6d77745b78e668f6d3fc2cf63b9ad801b6
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-09-28 23:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length; i++){
+		let char = str.charAt(i);
+	    // Check if the character appears only once in the string
+	    if (str.indexOf(char) === str.lastIndexOf(char)) {
+	      return char; // Found the first non-repeated character
+    }
+  }
+  return null; // Return null if there are no non-repeated characters
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
